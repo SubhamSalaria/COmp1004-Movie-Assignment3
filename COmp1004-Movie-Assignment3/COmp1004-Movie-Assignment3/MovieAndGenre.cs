a0aee1a5e0f2c3ea7acccff10bd057fe451ce76e
Shubam Salaria <Shubam.Salaria>
2017-03-05 17:57:20 -0500

    created order form and splash form

---
+++ COmp1004-Movie-Assignment3/COmp1004-Movie-Assignment3/MovieAndGenre.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COmp1004_Movie_Assignment3
+{
+    class MovieAndGenre
+    {
+        //Movie Title Info
+        public string Title { get; set; }
+
+        //Movie genre info
+        public string Genre { get; set; }
+
+        //Movie cost Info
+        public string Cost { get; set; }
+
+        //Movie Grand total info
+        public string GrandTotal { get; set; }
+
+        //Movie picture
+        public System.Drawing.Bitmap Picture { get; set; }
+
+    }
+}
---
+++ COmp1004-Movie-Assignment3/COmp1004-Movie-Assignment3/OrderForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COmp1004_Movie_Assignment3
+{
+    public partial class OrderForm : Form
+    {
+        SelectionForm select = new SelectionForm();
+        public OrderForm()
+        {
+            InitializeComponent();
+        }
+
+        //Cancel button closes the form
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //X closes the form
+        private void OrderForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //When the form loads populate with info saved from the last form
+        private void OrderForm_Load(object sender, EventArgs e)
+        {
+
+
+            TitleTextBox.Text = Program.info.Title;
+            GenreTextBox.Text = Program.info.Genre;
+            CostTextBox.Text = Program.info.Cost;
+            OrderPictureBox.Image = Program.info.Picture;
+
+            CalculateTotals();
+        }
+
+        //Method will calculate the totals of the movie depending on whether or not the add dvd box is checked
+        private void CalculateTotals()
+        {
+
+            if (OrderDvdCheckBox.Checked == true)
+            {
+                DvdLabel.Show();
+                DvdCostTextBox.Show();
+                double TotalTax = Convert.ToDouble((CostTextBox.Text as string).TrimStart('$'));
+                double DvdCost = TotalTax + 10;
+
+
+                TotalTax = DvdCost * 0.13;
+                double Grand = TotalTax + DvdCost;
+
+
+                SubtotalTextBox.Text = DvdCost.ToString("C2");
+                SalesTaxTextBox.Text = TotalTax.ToString("C2");
+                GrandTotalTextBox.Text = Grand.ToString("C2");
+                Program.info.GrandTotal = GrandTotalTextBox.Text;
+
+            }
+            else if (OrderDvdCheckBox.Checked == false)
+            {
+                DvdLabel.Hide();
+                DvdCostTextBox.Hide();
+                CostTextBox.Text = Program.info.Cost;
+                double TotalTax = Convert.ToDouble((CostTextBox.Text as string).TrimStart('$'));
+                double Grand = TotalTax * 1.13;
+                TotalTax = TotalTax * 0.13;
+
+
+
+                SubtotalTextBox.Text = CostTextBox.Text;
+                SalesTaxTextBox.Text = TotalTax.ToString("C2");
+                GrandTotalTextBox.Text = Grand.ToString("C2");
+                Program.info.GrandTotal = GrandTotalTextBox.Text;
+            }
+
+        }
+
+        //Back button returns you to the previous form.
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            select.Show();
+            this.Hide();
+            select.ReturnInfo();
+
+
+        }
+
+        //When check box is checked run the calculate totals method
+        private void OrderDvdCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+            CalculateTotals();
+
+
+
+        }
+
+        private void StreamButton_Click(object sender, EventArgs e)
+        {
+            StreamForm stream = new StreamForm();
+            stream.Show();
+            this.Hide();
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your receipt is being printed!", "Movie Bananza");
+
+        }
+
+        private void streamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamButton.PerformClick();
+        }
+
+        private void cancelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CancelButton.PerformClick();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Developer: Chandra Gundam" + "\r\n" +
+                "Version: 3.0" + "\r\n" + "Website: www.moviebonanza.com" + "\r\n" +
+                "About");
+        }
+    }
+}
