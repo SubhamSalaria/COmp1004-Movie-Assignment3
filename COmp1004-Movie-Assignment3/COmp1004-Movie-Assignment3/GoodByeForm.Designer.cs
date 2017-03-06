e9515a7faea3c49b56cb68482d8291ad40834179
Shubam Salaria <Shubam.Salaria>
2017-03-05 19:05:15 -0500

    created good bye form

---
+++ COmp1004-Movie-Assignment3/COmp1004-Movie-Assignment3/GoodByeForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace COmp1004_Movie_Assignment3
+{
+    partial class GoodByeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GoodByeLabel = new System.Windows.Forms.Label();
+            this.GoodByeTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // GoodByeLabel
+            // 
+            this.GoodByeLabel.AutoSize = true;
+            this.GoodByeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.GoodByeLabel.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GoodByeLabel.Location = new System.Drawing.Point(62, 56);
+            this.GoodByeLabel.Name = "GoodByeLabel";
+            this.GoodByeLabel.Size = new System.Drawing.Size(164, 23);
+            this.GoodByeLabel.TabIndex = 0;
+            this.GoodByeLabel.Text = "Happy Streaming!!";
+            // 
+            // GoodByeTimer
+            // 
+            this.GoodByeTimer.Interval = 3000;
+            this.GoodByeTimer.Tick += new System.EventHandler(this.GoodByeTimer_Tick);
+            // 
+            // GoodByeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(300, 133);
+            this.Controls.Add(this.GoodByeLabel);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "GoodByeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GoodByeForm";
+            this.Load += new System.EventHandler(this.GoodByeForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label GoodByeLabel;
+        private System.Windows.Forms.Timer GoodByeTimer;
+    }
+}
---
+++ COmp1004-Movie-Assignment3/COmp1004-Movie-Assignment3/GoodByeForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COmp1004_Movie_Assignment3
+{
+    public partial class GoodByeForm : Form
+    {
+        public GoodByeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GoodByeForm_Load(object sender, EventArgs e)
+        {
+            GoodByeTimer.Start();
+        }
+
+        private void GoodByeTimer_Tick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
